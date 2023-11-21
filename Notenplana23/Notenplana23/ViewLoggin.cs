5797d3fb2b38b3315c50976a1960e2c6817dc3a3
Mateusz Cabaj <mateuszcabaj@online.de>
2023-11-21 11:34:03 +0100

    testv9

--- Notenplana23/Notenplana23/ViewLoggin.cs
+++ Notenplana23/Notenplana23/ViewLoggin.cs
@@ -6,21 +6,5 @@ namespace Notenplana23
         {
             InitializeComponent();
         }
-
-        private void buttonLoggin_Click(Object sender, EventArgs e)
-		{
-            ViewHauptprogramm viewHauptprogramm = new ViewHauptprogramm();
-            ViewHauptprogramm.show();
-
-            this.Close();
-		}
-
-        private void buttonRegestrierung_Click(Object sender, EventArgs e)
-		{
-            ViewRegestrierung viewRegestrierung = new ViewRegestrierung();
-            viewRegestrierung.show();
-
-            this.Hide();
-		}
     }
 }
--- Notenplana23/Notenplana23/ViewRegestrierung.cs
+++ Notenplana23/Notenplana23/ViewRegestrierung.cs
@@ -16,31 +16,5 @@ namespace Notenplana23
         {
             InitializeComponent();
         }
-
-        private void textBoxBenutzername_textChanged(Object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBoxBenutzername_textChanged(Object sender, EventArgs e)
-        {
-
-        }
-
-        private void buttonZurueck_Click(Object sender, EventArgs e)
-		{
-            ViewLoggin viewLoggin = new ViewLoggin();
-            viewLoggin.show();
-
-            this.Close();
-		}
-
-        private void buttonSpeichern_Click(Object sender, EventArgs e)
-		{
-            ViewLoggin viewLoggin = new ViewLoggin();
-            viewLoggin.show();
-
-            this.Close();
-		}
     }
 }
